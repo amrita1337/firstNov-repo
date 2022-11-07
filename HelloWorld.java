2b78cf398560590307f143643beb80fc808204b2
amrita1337 <101690160+amrita1337@users.noreply.github.com>
2022-11-07 18:42:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!now completed first jenkins assignment");
+    System.out.println("Hello world!");
   }
 }
