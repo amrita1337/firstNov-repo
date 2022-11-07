102684a11261efdf7ddba807fa36c2526a4ca0e4
amrita1337 <101690160+amrita1337@users.noreply.github.com>
2022-11-07 19:00:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,7 @@
-public class Main {
-
-  public static void main(String[] args) {
-
-    int num = 29;
-    boolean flag = false;
-    for (int i = 2; i <= num / 2; ++i) {
-      // condition for nonprime number
-      if (num % i == 0) {
-        flag = true;
-        break;
-      }
+public class Hello{
+  public static void main(String[] args){
+    for(int i=1; i<=10;i++){
+      System.outprintIn("Hello World ... "+i);
     }
-
-    if (!flag)
-      System.out.println(num + " is a prime number.");
-    else
-      System.out.println(num + " is not a prime number.");
   }
 }
